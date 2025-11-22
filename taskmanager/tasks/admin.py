ce06262a00f05f57509d593634e2edc6f1d39cdf
Anik Ibne Sayed <anikhossen3422@gmail.com>
2025-11-22 14:46:34 +0600

    fix some issue

--- taskmanager/tasks/admin.py
+++ taskmanager/tasks/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Task
+
+# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title','description','status','priority','created_at', 'updated_at', 'due_date','owner')
+
+    ordering = ('-created_at',)
